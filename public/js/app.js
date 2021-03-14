d611a6c81160eb19920cb56de2b6931e593d5c1f
vagrant <vagrant@homestead>
2021-03-14 14:59:31 +0000

    nieuwe build

--- public/js/app.js
+++ public/js/app.js
@@ -37353,8 +37353,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Philip\web_development\dreams\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Philip\web_development\dreams\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Philip\web_development\Dreams\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Philip\web_development\Dreams\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
